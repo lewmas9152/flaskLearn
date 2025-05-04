a7ab1882425e654bd7f3f5ad72416e58acf428ce
lewmas9152 <smnjuguna22@gmail.com>
2025-05-04 11:45:18 +0300

    Used bootstrap for styling a registration form

--- 14flaskDays/app/__init__.py
+++ 14flaskDays/app/__init__.py
@@ -2,18 +2,16 @@ from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return "<p>Hello</p>"
-
-@app.route("/form", methods= ["POST", "GET"])
+@app.route("/", methods= ["POST", "GET"])
 def form():
     if request.method == "POST":
         name = request.form.get('name')
         email = request.form.get('email')
+        password = request.form.get("Password")
         data = {
             "name": name,
-            "email":email
+            "email":email,
+            "password":password
         }
         return data
 
